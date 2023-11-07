f46a49212d2a8fc52231a5b97517375248a042e0
Nemes Tamás <nemes.tamas@mechwart.local>
2023-11-07 17:48:40 +0100

    models created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Invoice, User
 
-# Register your models here.
+admin.site.register(Invoice)
+admin.site.register(User)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Invoice(models.Model):
+    statuses = [
+        (0,'Draft'),
+        (1,'Pending'),
+        (2,'Paid')
+    ]
+
+    status = models.IntegerField(choices = statuses, default=0)
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount_in_huf = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.date} - status: {self.status}"
